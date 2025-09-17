e00b50b6343c0a914b6b86a3721af8a35e9b4dfe
Ghafoor1111 <ga810919@gmail.com>
2025-09-17 15:19:12 +0500

    Create expired.js

---
+++ pages/pages/api/pages/s/pages/pages/pages/expired.js
@@ -0,0 +1,9 @@
+// pages/expired.js
+export default function Expired() {
+  return (
+    <div style={{padding:40, fontFamily:"system-ui"}}>
+      <h1>Link expired</h1>
+      <p>This link is no longer valid or has expired.</p>
+    </div>
+  );
+}
